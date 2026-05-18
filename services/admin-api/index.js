00955f5bf161b2a73a45fde6146623ff4a5da363
Vaibhav senta <vaibhavsenta999@icloud.com>
2026-05-18 12:21:53 -0700

    update

--- services/admin-api/index.js
+++ services/admin-api/index.js
@@ -13,7 +13,7 @@ const globalErrorHandler = require('./middlewares/errorMiddleware');
 
 
 const port = process.env.PORT || 5001;
-const connectionString = process.env.CONNECTION_URI || "mongodb://localhost:27017/cloudbase"
+const connectionString = process.env.CONNECTION || "mongodb://localhost:27017/cloudbase"
 
 
 
