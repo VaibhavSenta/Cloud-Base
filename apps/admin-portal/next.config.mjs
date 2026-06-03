f462968ae0e0b4c3043861e3eabf49e2bb316005
Vaibhav senta <vaibhavsenta999@icloud.com>
2026-06-02 22:44:16 -0700

    Update next.config.mjs

--- apps/admin-portal/next.config.mjs
+++ apps/admin-portal/next.config.mjs
@@ -3,6 +3,9 @@ import withPWAInit from 'next-pwa';
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  buildExhaustive: true,
 });
 
 /** @type {import('next').NextConfig} */
