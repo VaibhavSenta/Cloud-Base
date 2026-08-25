0ff1bb60c1b69838d0b8fbc6e58055aaafe7f135
Vaibhav Senta <vaibhavsenta.cloudbase@gmail.com>
2026-08-26 04:28:48 +0530

    style: smooth out triangle corner radiuses using SVG Bezier curves (M Q L Z) for perfect scaling at all sizes

--- apps/chat-portal/src/components/UI/NothingboxLogo/NothingboxLogo.js
+++ apps/chat-portal/src/components/UI/NothingboxLogo/NothingboxLogo.js
@@ -14,42 +14,49 @@ export default function NothingboxLogo({ size = 36, className = '' }) {
         xmlns="http://www.w3.org/2000/svg"
         className={styles.logoSvg}
       >
-        {/* 1. Backmost Triangle 1: Pointing DOWN (Dark Grey Outline) */}
-        <polygon 
-          points="250,460 70,140 430,140" 
-          stroke="#55555e" 
-          strokeWidth="18" 
+        <defs>
+          <linearGradient id="nbWhiteGlow" x1="250" y1="50" x2="250" y2="400" gradientUnits="userSpaceOnUse">
+            <stop offset="0%" stopColor="#ffffff" />
+            <stop offset="100%" stopColor="#e5e5ea" />
+          </linearGradient>
+        </defs>
+
+        {/* 1. Backmost Downward Triangle (Soft Smooth Rounded Dark Grey Outline) */}
+        <path 
+          d="M 230,445 Q 250,470 270,445 L 420,165 Q 435,140 410,140 L 90,140 Q 65,140 80,165 Z" 
+          stroke="#48484a" 
+          strokeWidth="20" 
           strokeLinejoin="round" 
           strokeLinecap="round" 
           fill="none" 
         />
 
-        {/* 2. Middle Triangle 2: Rotated / Tilted (Mid Grey Outline) */}
-        <polygon 
-          points="60,280 390,60 390,440" 
+        {/* 2. Middle Tilted Triangle (Soft Smooth Rounded Mid Grey Outline) */}
+        <path 
+          d="M 75,265 Q 55,280 75,295 L 375,435 Q 395,445 395,420 L 395,80 Q 395,55 375,65 Z" 
           stroke="#8e8e93" 
-          strokeWidth="18" 
+          strokeWidth="20" 
           strokeLinejoin="round" 
           strokeLinecap="round" 
           fill="none" 
         />
 
-        {/* 3. Foremost Main Triangle: Pointing UP (White Outer Border & White Fill) */}
-        <polygon 
-          points="250,60 440,390 60,390" 
-          stroke="#ffffff" 
+        {/* 3. Foremost Upward Triangle (Smooth Rounded Outer White Border & Fill) */}
+        <path 
+          d="M 230,75 Q 250,50 270,75 L 425,370 Q 440,395 415,395 L 85,395 Q 60,395 75,370 Z" 
+          stroke="url(#nbWhiteGlow)" 
           strokeWidth="24" 
           strokeLinejoin="round" 
           strokeLinecap="round" 
           fill="#ffffff" 
         />
 
-        {/* 4. Inner Stitched Dashed Line inside Main Triangle */}
-        <polygon 
-          points="250,105 405,365 95,365" 
-          stroke="#666666" 
-          strokeWidth="8" 
-          strokeDasharray="12 10"
+        {/* 4. Inner Stitched Dotted Contour Line (Silky Smooth Rounded Curve Corner) */}
+        <path 
+          d="M 235,115 Q 250,95 265,115 L 395,360 Q 405,378 385,378 L 115,378 Q 95,378 105,360 Z" 
+          stroke="#48484a" 
+          strokeWidth="10" 
+          strokeDasharray="14 10"
           strokeLinejoin="round" 
           strokeLinecap="round" 
           fill="none" 
